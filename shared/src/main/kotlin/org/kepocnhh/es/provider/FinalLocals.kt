711d7731f5647b9932da2d1306f6e592681fe445
Stanley Wintergreen <kepocnhh@protonmail.com>
2025-05-15 21:02:09 +0300

    main...

--- shared/src/main/kotlin/org/kepocnhh/es/provider/FinalLocals.kt
+++ shared/src/main/kotlin/org/kepocnhh/es/provider/FinalLocals.kt
@@ -1,6 +1,8 @@
 package org.kepocnhh.es.provider
 
 import org.kepocnhh.es.entity.Keys
+import sp.kx.bytes.readInt
+import sp.kx.bytes.write
 import java.io.File
 
 internal class FinalLocals : Locals {
@@ -14,9 +16,35 @@ internal class FinalLocals : Locals {
         get() {
             val file = dir.resolve("keys")
             if (!file.exists()) return null
-            TODO("FinalLocals:keys:get")
+            val bytes = file.readBytes()
+            var index = 0
+            val publicKey = ByteArray(bytes.readInt(index = index))
+            index += 4
+            System.arraycopy(bytes, index, publicKey, 0, publicKey.size)
+            index += publicKey.size
+            val privateKeyEncrypted = ByteArray(bytes.readInt(index = index))
+            index += 4
+            System.arraycopy(bytes, index, privateKeyEncrypted, 0, privateKeyEncrypted.size)
+            return Keys(
+                publicKey = publicKey,
+                privateKeyEncrypted = privateKeyEncrypted,
+            )
         }
         set(value) {
-            TODO("FinalLocals:keys:set($value)")
+            val file = dir.resolve("keys")
+            if (value == null) {
+                file.delete()
+            } else {
+                val bytes = ByteArray(4 + value.publicKey.size + 4 + value.privateKeyEncrypted.size)
+                var index = 0
+                bytes.write(index = index, value.publicKey.size)
+                index += 4
+                System.arraycopy(value.publicKey, 0, bytes, index, value.publicKey.size)
+                index += value.publicKey.size
+                bytes.write(index = index, value.privateKeyEncrypted.size)
+                index += 4
+                System.arraycopy(value.privateKeyEncrypted, 0, bytes, index, value.privateKeyEncrypted.size)
+                file.writeBytes(bytes)
+            }
         }
 }
